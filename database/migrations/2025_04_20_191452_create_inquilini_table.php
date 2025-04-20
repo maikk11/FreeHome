f7b03d43eebe85fa53bd920915f3e63121d54b9c
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2025-04-20 21:41:28 +0200

    creata struttura per inquilini

---
+++ database/migrations/2025_04_20_191452_create_inquilini_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inquilini', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 255);
+            $table->string('cognome', 255);
+            $table->string('carta_identità', 25);
+            $table->string('codice_fiscale', 25);
+            $table->date('data_nascita');
+            $table->string('provincia_nascita', 50);
+            $table->string('comune_nascita', 50);
+            $table->string('provincia_residenza', 50);
+            $table->string('comune_residenza', 50);
+            $table->string('email')->unique();
+            $table->string('numero_telefono', 20);
+            $table->date('data_subentro');
+            $table->date('data_uscita');
+            $table->string('contratto_lavorativo', 255);
+            $table->integer('numero_stanza');
+            $table->unsignedBigInteger('immobile_id');
+            $table->foreign('immobile_id')->references('id')->on('immobili');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inquilini');
+    }
+};
